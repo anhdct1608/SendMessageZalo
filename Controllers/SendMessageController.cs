7a24a2581d14949c6625055fc405b4d4e1960741
anhdct1608 <anhdct1608@gmail.com>
2021-08-02 09:18:05 +0700

    update

--- Controllers/SendMessageController.cs
+++ Controllers/SendMessageController.cs
@@ -37,46 +37,31 @@ namespace SendMessageZalo.Controllers
             string secretKey = Configuration.GetSection("SendMessageZalo:secretKey").Value;
             string callbackUrl = Configuration.GetSection("SendMessageZalo:callbackUrl").Value;
             string code = Configuration.GetSection("SendMessageZalo:code").Value;
-
             ZaloAppInfo appInfo = new ZaloAppInfo(appId, secretKey, callbackUrl);
             ZaloAppClient appClient = new ZaloAppClient(appInfo);
             String loginUrl = appClient.getLoginUrl();
-
             JObject token = appClient.getAccessToken(code);
             string access_token = token["access_token"].ToString();
-
             JObject profile = appClient.getProfile(access_token, "id, name, birthday");
-            ZaloClient client = new ZaloClient(token["access_token"].ToString());
-            JObject follower = client.getListFollower(0, 20);
-
             string friendsUrls = "https://graph.zalo.me/v2.0/me/friends?access_token="+access_token+"&offset=0&limit=5&fields=id,name,gender,picture";
             string Invitable_friendsUrls = "https://graph.zalo.me/v2.0/me/invitable_friends?access_token=" + access_token + "&offset=0&limit=100&fields=id,name,gender,picture";
             string ApprequestUrl = "https://graph.zalo.me/v2.0/apprequests?access_token=" + access_token + "&message="+msg+"&to="+listUserId;
             
-            Object Invitable_friends = _webClient.DownloadString(Invitable_friendsUrls);
-            Object friends = _webClient.DownloadString(friendsUrls);
+            //Object Invitable_friends = _webClient.DownloadString(Invitable_friendsUrls);
+            //Object friends = _webClient.DownloadString(friendsUrls);
             //Object Apprequest_Result = _webClient.UploadString(ApprequestUrl,"");
             JArray result = new JArray();
             //------ Gửi tin nhắn
             foreach (string UserId in listUserId){
                 string SendMessageUrls = "https://graph.zalo.me/v2.0/me/message?access_token="+ access_token + "&message="+msg+"&to="+UserId;
-                Object result2 = _webClient.UploadString(SendMessageUrls,"");
-                result.Add(result2);
+                Object SendMessage = _webClient.UploadString(SendMessageUrls,"");
+                result.Add(SendMessage);
             }
             //------
             JObject res = new JObject();
             res.Add("ok", true);
             res.Add("data", result.ToString());
             return res;
-        }
-
-        [HttpGet("GetToken/{uid}/{code}/{state}/{scope}")]
-        public string GetToken(long uid, string code,string state,JObject scope)
-        {
-            JObject res = new JObject();
-            res.Add("ok", true);
-            //res.Add("data", model.ToString());
-            return code;
         }
     }
 }
